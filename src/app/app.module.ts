8bfee87bbe8163872844e8308e0fa47dfdc18d15
Hari <harikrishkk@gmail.com>
2023-04-28 18:57:13 -0400

    commit 2: banner + navbar + footer components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
